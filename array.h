dc03d457a22df10233b5ef6f5dc293093b5e1f39
TotoooDev <totooodev@proton.me>
2025-03-05 16:47:17 +0100

    array: did lots of stuff

--- array.h
+++ array.h
@@ -49,58 +49,57 @@
  * deletes `array` and frees all related memory.
  * all pointers to `array` become invalid after this call.
  */
-#define array_delete(array) if ((array)->buffer != NULL) { free((array)->buffer); } free(array)
+#define array_delete(array) do { if ((array).buffer != NULL) { free((array).buffer); } } while (0)
 
 /**
  * increases the capacity of the array. this does nothing if `new_size` is smaller than the current capacity.
  */
-#define array_reserve(array, new_size) if ((new_size) > (array)->capacity) { (array)->buffer = malloc((new_size) * (array)->elem_size); (array)->capacity = new_size; }
+#define array_reserve(array, new_size) if ((new_size) > (array).capacity) { (array).buffer = malloc((new_size) * (array).elem_size); (array).capacity = new_size; }
 
 /**
  * reallocates the array buffer to be as small as possible.
  */
-#define array_shrink(array) (array)->buffer = realloc((array)->buffer, (array)->elem_size * (array)->length); (array)->capacity = (array)->length
+#define array_shrink(array) (array).buffer = realloc((array).buffer, (array).elem_size * (array).length); (array).capacity = (array).length
 
 /**
  * removes the last element in the array.
  */
-#define array_pop(array) array_remove_index((array), (array)->length - 1)
+#define array_pop(array) array_remove_index((array), (array).length - 1)
 
 /**
  * returns the element located at index `index`.
- * this does NOT check bounds!
  */
-#define array_get(array, index) assert((index) > 0 && (index) < (array)->length); (array)->buffer[(index)]
+#define array_get(array, index) (assert((index) >= 0 && (index) < (array).length), (array).buffer[(index)])
 
 /**
  * returns the first element in the array.
  */
-#define array_front(array) (array)->buffer[0]
+#define array_front(array) (array).buffer[0]
 
 /**
  * returns the last element in the array.
  */
-#define array_back(array) (array)->buffer[(array)->length - 1]
+#define array_back(array) (array).buffer[(array).length - 1]
 
 /**
  * returns whether or not the array is empty.
  */
-#define array_is_empty(array) (array)->length == 0
+#define array_is_empty(array) (array).length == 0
 
 /**
  * returns the capacity of the array buffer, that is the maximum number of elements the array can contain before needing to be resized.
  */
-#define array_get_capacity(array) (array)->capacity
+#define array_get_capacity(array) (array).capacity
 
 /**
  * returns the number of elements in the array.
  */
-#define array_get_length(array) (array)->length
+#define array_get_length(array) (array).length
 
 /**
  * iterates through all the elements in the array. `elem` is set to the current element, `i` is set to the index of the element.
  */
-#define array_foreach(array, elem, i) for (i = 0, elem = (array)->buffer[0]; i < (array)->length; i++, elem = (array)->buffer[i])
+#define array_foreach(array, elem, i) for (i = 0, elem = (array).buffer[0]; i < (array).length; i++, elem = (array).buffer[i])
 
 /**
  * initializes the array.
@@ -108,38 +107,37 @@
  * `func` is the function that compares two elements. you don't need to define it and can simply set it to `NULL`, but you won't be able to use `array_get_index` and `array_remove`.
  */
 #define array_init(array, type, func) do { \
-    array = malloc(sizeof(array_t(type))); \
-    (array)->buffer = malloc(sizeof(type) * 5); \
-    (array)->capacity = 5; \
-    (array)->elem_size = sizeof(type); \
-    (array)->length = 0; \
-    (array)->compare_function = func; \
+    (array).buffer = malloc(sizeof(type) * 5); \
+    (array).capacity = 5; \
+    (array).elem_size = sizeof(type); \
+    (array).length = 0; \
+    (array).compare_function = func; \
 } while (0)
 
 /**
  * adds an element in the array.
  */
 #define array_add(array, elem) do { \
-    if ((array)->capacity <= (array)->length) { \
-        (array)->capacity += (array)->capacity / 2 + 1; \
-        (array)->buffer = realloc((array)->buffer, (array)->elem_size * (array)->capacity); \
+    if ((array).capacity <= (array).length) { \
+        (array).capacity += (array).capacity / 2 + 1; \
+        (array).buffer = realloc((array).buffer, (array).elem_size * (array).capacity); \
     } \
-    (array)->buffer[(array)->length] = elem; \
-    (array)->length++; \
+    (array).buffer[(array).length] = elem; \
+    (array).length++; \
 } while (0)
 
 /**
  * insert an element at the given index.
  */
 #define array_insert(array, elem, index) do { \
-    assert((index) > 0 && (index) < (array)->length); \
-    if ((array)->capacity <= (array)->length) { \
-        (array)->capacity += (array)->capacity / 2 + 1; \
-        (array)->buffer = realloc((array)->buffer, (array)->elem_size * (array)->capacity); \
+    assert((index) > 0 && (index) < (array).length); \
+    if ((array).capacity <= (array).length) { \
+        (array).capacity += (array).capacity / 2 + 1; \
+        (array).buffer = realloc((array).buffer, (array).elem_size * (array).capacity); \
     } \
-    memmove(&(array)->buffer[(index) + 1], &(array)->buffer[(index)], (array)->length - (index)); \
-    (array)->buffer[(index)] = elem; \
-    (array)->length++; \
+    memmove(&(array).buffer[(index) + 1], &(array).buffer[(index)], (array).length - (index)); \
+    (array).buffer[(index)] = elem; \
+    (array).length++; \
 } while (0)
 
 /**
@@ -147,12 +145,12 @@
  * you need to have specified a compare function in `array_init` to use this macro.
  */
 #define array_get_index(array, elem, i) do { \
-    for (i = 0; i < (array)->length; i++) { \
-        if ((array)->compare_function((array)->buffer[i], elem)) { \
+    for (i = 0; i < (array).length; i++) { \
+        if ((array).compare_function((array).buffer[i], elem)) { \
             break; \
         } \
     } \
-    if (i == (array)->length) { \
+    if (i == (array).length) { \
         i = -1; \
     } \
 } while (0)
@@ -161,12 +159,12 @@
  * removes the element located at `index` in the array.
  */
 #define array_remove_index(array, index) do { \
-    assert((index) > 0 && (index) < (array)->length); \
-    if ((index) < (array)->length) { \
-        for (size_t i = (index); i < (array)->length - 1; i++) { \
-            (array)->buffer[i] = (array)->buffer[i + 1]; \
+    assert((index) > 0 && (index) < (array).length); \
+    if ((index) < (array).length) { \
+        for (size_t i = (index); i < (array).length - 1; i++) { \
+            (array).buffer[i] = (array).buffer[i + 1]; \
         } \
-        (array)->length--; \
+        (array).length--; \
     } \
 } while (0)
 
@@ -186,9 +184,9 @@
  * all contents will be erased!
  */
 #define array_clear(array) do { \
-    (array)->buffer = realloc((array)->buffer, (array)->elem_size); \
-    (array)->capacity = 1; \
-    (array)->length = 0; \
+    (array).buffer = realloc((array).buffer, (array).elem_size); \
+    (array).capacity = 1; \
+    (array).length = 0; \
 } while (0)
 
 // ==============================================
@@ -197,6 +195,54 @@
 
 // no i'm not writing docs for all of this
 // sorry :)
+
+bool unsigned_int_compare(unsigned int a, unsigned int b);
+bool int_compare(int a, int b);
+bool uint8_compare(uint8_t a, uint8_t b);
+bool uint16_compare(uint16_t a, uint16_t b);
+bool uint32_compare(uint32_t a, uint32_t b);
+bool uint64_compare(uint64_t a, uint64_t b);
+bool int8_compare(int8_t a, int8_t b);
+bool int16_compare(int16_t a, int16_t b);
+bool int32_compare(int32_t a, int32_t b);
+bool int64_compare(int64_t a, int64_t b);
+bool float_compare(float a, float b);
+bool double_compare(double a, double b);
+bool char_compare(char a, char b);
+bool str_compare(char* a, char* b);
+bool void_compare(void* a, void* b);
+
+typedef array_t(unsigned int)   unsigned_int_array_t;
+typedef array_t(int)            int_array_t;
+typedef array_t(uint8_t)        uint8_array_t;
+typedef array_t(uint16_t)       uint16_array_t;
+typedef array_t(uint32_t)       uint32_array_t;
+typedef array_t(uint64_t)       uint64_array_t;
+typedef array_t(int8_t)         int8_array_t;
+typedef array_t(int16_t)        int16_array_t;
+typedef array_t(int32_t)        int32_array_t;
+typedef array_t(int64_t)        int64_array_t;
+typedef array_t(float)          float_array_t;
+typedef array_t(double)         double_array_t;
+typedef array_t(char)           char_array_t;
+typedef array_t(char*)          str_array_t;
+typedef array_t(void*)          void_array_t;
+
+unsigned_int_array_t   unsigned_int_array_new();
+int_array_t            int_array_new();
+uint8_array_t          uint8_array_new();
+uint16_array_t         uint16_array_new();
+uint32_array_t         uint32_array_new();
+uint64_array_t         uint64_array_new();
+int8_array_t           int8_array_new();
+int16_array_t          int16_array_new();
+int32_array_t          int32_array_new();
+int64_array_t          int64_array_new();
+float_array_t          float_array_new();
+double_array_t         double_array_new();
+char_array_t           char_array_new();
+str_array_t            str_array_new();
+void_array_t           void_array_new();
 
 #ifdef TCL_DEFINE_ARRAYS
 
@@ -216,37 +262,21 @@ bool char_compare(char a, char b) { return a == b; }
 bool str_compare(char* a, char* b) { return strcmp(a, b) == 0; }
 bool void_compare(void* a, void* b) { return a == b; }
 
-typedef array_t(unsigned int)   array_unsigned_int_t;
-typedef array_t(int)            array_int_t;
-typedef array_t(uint8_t)        array_uint8_t;
-typedef array_t(uint16_t)       array_uint16_t;
-typedef array_t(uint32_t)       array_uint32_t;
-typedef array_t(uint64_t)       array_uint64_t;
-typedef array_t(int8_t)         array_int8_t;
-typedef array_t(int16_t)        array_int16_t;
-typedef array_t(int32_t)        array_int32_t;
-typedef array_t(int64_t)        array_int64_t;
-typedef array_t(float)          array_float_t;
-typedef array_t(double)         array_double_t;
-typedef array_t(char)           array_char_t;
-typedef array_t(char*)          array_str_t;
-typedef array_t(void*)          array_void_t;
-
-array_unsigned_int_t*   array_unsigned_int_new() { array_unsigned_int_t* a; array_init(a, unsigned int, unsigned_int_compare); return a; }
-array_int_t*            array_int_new() { array_int_t* a; array_init(a, int, int_compare); return a; }
-array_uint8_t*          array_uint8_new()  { array_uint8_t* a;  array_init(a, uint8_t,  uint8_compare);  return a; }
-array_uint16_t*         array_uint16_new() { array_uint16_t* a; array_init(a, uint16_t, uint16_compare); return a; }
-array_uint32_t*         array_uint32_new() { array_uint32_t* a; array_init(a, uint32_t, uint32_compare); return a; }
-array_uint64_t*         array_uint64_new() { array_uint64_t* a; array_init(a, uint64_t, uint64_compare); return a; }
-array_int8_t*           array_int8_new()  { array_int8_t* a;  array_init(a, int8_t,  int8_compare);  return a; }
-array_int16_t*          array_int16_new() { array_int16_t* a; array_init(a, int16_t, int16_compare); return a; }
-array_int32_t*          array_int32_new() { array_int32_t* a; array_init(a, int32_t, int32_compare); return a; }
-array_int64_t*          array_int64_new() { array_int64_t* a; array_init(a, int64_t, int64_compare); return a; }
-array_float_t*          array_float_new()  { array_float_t* a;  array_init(a, float, float_compare);   return a; }
-array_double_t*         array_double_new() { array_double_t* a; array_init(a, double, double_compare); return a; }
-array_char_t*           array_char_new() { array_char_t* a; array_init(a, char, char_compare); return a; }
-array_str_t*            array_str_new() { array_str_t* a; array_init(a, char*, str_compare); return a; }
-array_void_t*           array_void_new() { array_void_t* a; array_init(a, void*, void_compare); return a; }
+unsigned_int_array_t   unsigned_int_array_new() { unsigned_int_array_t a; array_init(a, unsigned int, unsigned_int_compare); return a; }
+int_array_t            int_array_new() { int_array_t a; array_init(a, int, int_compare); return a; }
+uint8_array_t          uint8_array_new()  { uint8_array_t a;  array_init(a, uint8_t,  uint8_compare);  return a; }
+uint16_array_t         uint16_array_new() { uint16_array_t a; array_init(a, uint16_t, uint16_compare); return a; }
+uint32_array_t         uint32_array_new() { uint32_array_t a; array_init(a, uint32_t, uint32_compare); return a; }
+uint64_array_t         uint64_array_new() { uint64_array_t a; array_init(a, uint64_t, uint64_compare); return a; }
+int8_array_t           int8_array_new()  { int8_array_t a;  array_init(a, int8_t,  int8_compare);  return a; }
+int16_array_t          int16_array_new() { int16_array_t a; array_init(a, int16_t, int16_compare); return a; }
+int32_array_t          int32_array_new() { int32_array_t a; array_init(a, int32_t, int32_compare); return a; }
+int64_array_t          int64_array_new() { int64_array_t a; array_init(a, int64_t, int64_compare); return a; }
+float_array_t          float_array_new()  { float_array_t a;  array_init(a, float, float_compare);   return a; }
+double_array_t         double_array_new() { double_array_t a; array_init(a, double, double_compare); return a; }
+char_array_t           char_array_new() { char_array_t a; array_init(a, char, char_compare); return a; }
+str_array_t            str_array_new() { str_array_t a; array_init(a, char*, str_compare); return a; }
+void_array_t           void_array_new() { void_array_t a; array_init(a, void*, void_compare); return a; }
 
 #endif
 
